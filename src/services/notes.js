ba3760586b1da8f2db1ee056123aa041771ee8d7
fanik05 <fatinanik129@gmail.com>
2022-03-21 15:15:20 +0600

    base url updated

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://notes-backend-fanik.herokuapp.com/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () =>
   axios.get(baseUrl).then(response =>
@@ -8,7 +8,7 @@ const getAll = () =>
       id: 10000,
       content: 'This note is not saved to server',
       date: '2019-05-30T17:30:31.098Z',
-      important: true,
+      important: true
     })
   )
 
